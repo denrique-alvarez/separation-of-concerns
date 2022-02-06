a04d79747f173dd05ae70900258e0c86572ea843
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-06 16:27:17 +0100

    Update pass-tests-2.js

--- isolate/01-dom-elements/exercises/pass-tests-2.js
+++ isolate/01-dom-elements/exercises/pass-tests-2.js
@@ -1,6 +1,11 @@
 'use strict';
 
 // the assertions are correct!  write code above to pass them
+
+const element = document.createElement('p');
+element.innerHTML = 'lorem ipsum dolor';
+element.className = 'fancy-text';
+
 console.assert(element.nodeName === 'P', 'Test 1: nodeName');
 console.assert(element.innerHTML === 'lorem ipsum dolor', 'Test 2: innerHTML');
 console.assert(element.className === 'fancy-text', 'Test 3: className');
