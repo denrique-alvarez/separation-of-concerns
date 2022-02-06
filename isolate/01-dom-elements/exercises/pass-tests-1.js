b75b481f1956a8876327c79b34b43e2321c1c4da
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-06 16:27:15 +0100

    Update pass-tests-1.js

--- isolate/01-dom-elements/exercises/pass-tests-1.js
+++ isolate/01-dom-elements/exercises/pass-tests-1.js
@@ -1,9 +1,14 @@
 'use strict';
 
 // the assertions are correct!  write code above to pass them
+
+const element = document.createElement('input');
+element.placeHolder = 'enter your username';
+element.className = 'auth-field';
+
 console.assert(element.nodeName === 'INPUT', 'Test 1: nodeName');
 console.assert(
-  element.placeholder === 'enter your username',
+  element.placeHolder === 'enter your username',
   'Test 2: placeholder',
 );
 console.assert(element.className === 'auth-field', 'Test 3: className');
