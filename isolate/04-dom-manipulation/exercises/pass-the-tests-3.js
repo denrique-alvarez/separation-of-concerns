702e0a60cd77992249f747bca9e628d282e6b458
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-10 20:15:26 +0100

    Update pass-the-tests-3.js

--- isolate/04-dom-manipulation/exercises/pass-the-tests-3.js
+++ isolate/04-dom-manipulation/exercises/pass-the-tests-3.js
@@ -13,6 +13,18 @@ console.log(divEl.nodeName + ' (before)', divEl.cloneNode(true));
 
 // --- write some code ---
 //  you want to create a 2x2 table with a, b, c, d in the squares
+const tdEla = document.createElement('td');
+tdEla.innerHTML = 'a';
+divEl.children[0].children[0].children[0].appendChild(tdEla);
+const tdElb = document.createElement('td');
+tdElb.innerHTML = 'b';
+divEl.children[0].children[0].children[0].appendChild(tdElb);
+const tdElc = document.createElement('td');
+tdElc.innerHTML = 'c';
+divEl.children[0].children[0].children[1].appendChild(tdElc);
+const tdEld = document.createElement('td');
+tdEld.innerHTML = 'd';
+divEl.children[0].children[0].children[1].appendChild(tdEld);
 
 // --- --- --- --- --- ---
 
