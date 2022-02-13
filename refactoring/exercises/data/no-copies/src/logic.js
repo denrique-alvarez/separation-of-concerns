1c79a94db12bcd5db5f5788d6ffab18e5079e88c
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-13 16:14:51 +0100

    Update logic.js

--- refactoring/exercises/data/no-copies/src/logic.js
+++ refactoring/exercises/data/no-copies/src/logic.js
@@ -1,4 +1,12 @@
 /**
  *
  */
-export const saveNoCopies = () => {};
+
+import { data } from './data.js';
+
+export const saveNoCopies = () => {
+    const alreadySaved = data.noCopies.includes(userInput);
+    if (!alreadySaved) {
+        data.noCopies.push(userInput);
+    }
+};
