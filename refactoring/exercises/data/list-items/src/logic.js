223b17da0a8734f85d11b77ed69e46a2e629a524
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-13 16:14:33 +0100

    Update logic.js

--- refactoring/exercises/data/list-items/src/logic.js
+++ refactoring/exercises/data/list-items/src/logic.js
@@ -1,4 +1,12 @@
 /**
  *
  */
-export const list = () => {};
+import { bulletPoint } from './data.js';
+
+export const list = (allInputs) => {
+    let stringList = '';
+    for (const input of allInputs) {
+        stringList += `\n${bulletPoint} ${input}`;
+    }
+    return stringList;
+};
