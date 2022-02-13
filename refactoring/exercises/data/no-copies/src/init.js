ac07dbe6ff2ede529646915454fe8bdc9cbfda56
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-13 16:14:44 +0100

    Update init.js

--- refactoring/exercises/data/no-copies/src/init.js
+++ refactoring/exercises/data/no-copies/src/init.js
@@ -1,25 +1,2 @@
 // data to refactor out of this file
-const data = {
-  noCopies: [],
-};
-
-document.getElementById('no-copies-button').addEventListener('click', () => {
-  // read & process user input
-  let userInput = null;
-  while (userInput === null) {
-    userInput = prompt('enter a string to save');
-  }
-
-  // execute core logic
-  const alreadySaved = data.noCopies.includes(userInput);
-  if (!alreadySaved) {
-    data.noCopies.push(userInput);
-  }
-
-  // communicate result to user
-  const message = data.noCopies.join(', ');
-  alert(message);
-
-  // log interaction
-  console.log(data);
-});
+import './listener.js';
