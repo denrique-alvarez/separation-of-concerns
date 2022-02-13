87d19650fb17f0c12dc33a05737fabdfb669f0a7
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-13 16:14:40 +0100

    Update handler.js

--- refactoring/exercises/data/no-copies/src/handler.js
+++ refactoring/exercises/data/no-copies/src/handler.js
@@ -1,4 +1,20 @@
-import { saveNoCopies } from './utils.js';
+import { saveNoCopies } from './logic.js';
 import { data } from './data.js';
 
-export const saveNoCopiesHandler = () => {};
+export const saveNoCopiesHandler = () => {
+    // read & process user input
+    let userInput = null;
+    while (userInput === null) {
+        userInput = prompt('enter a string to save');
+    }
+
+    // execute core logic
+
+
+    // communicate result to user
+    const message = data.noCopies.join(', ');
+    alert(message);
+
+    // log interaction
+    console.log(data);
+}
