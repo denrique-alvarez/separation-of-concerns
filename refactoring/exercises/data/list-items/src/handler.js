c954da8543b4c61968d7d5f2160aab4331daeb2b
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-13 16:14:23 +0100

    Update handler.js

--- refactoring/exercises/data/list-items/src/handler.js
+++ refactoring/exercises/data/list-items/src/handler.js
@@ -1,4 +1,23 @@
-import { list } from './utils.js';
+import { list } from './logic.js';
 import { bulletPoint } from './data.js';
 
-export const listHandler = () => {};
+export const listHandler = () => {
+    // read & process user input
+    const allInputs = [];
+    let acceptingInput = true;
+    while (acceptingInput) {
+        const nextInput = prompt('enter a list item');
+        if (nextInput !== null) {
+            allInputs.push(nextInput);
+        } else {
+            acceptingInput = false;
+        }
+    }
+
+    // execute core logic
+    let stringList = list(allInputs);
+
+    // communicate result to user
+    const message = `all items:${stringList}`;
+    alert(message);
+};
