5a337505ec7b70b41985136d73dc5bf3a27d0b08
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-21 19:28:48 +0100

    Update handler.js

--- refactoring/exercises/data/running-average/src/handler.js
+++ refactoring/exercises/data/running-average/src/handler.js
@@ -1,4 +1,27 @@
-import { average } from './utils.js';
+import { average } from './logic.js';
 import { data } from './data.js';
 
-export const runningAverageHandler = () => {};
+export const runningAverageHandler = () => {
+    // read & process user input
+    const userInput = prompt('add a number to the running average');
+    if (userInput === null) {
+        alert('good bye');
+        return;
+    }
+
+    const newNumber = Number(userInput);
+    if (Number.isNaN(newNumber) || userInput === '') {
+        alert(`"${userInput}" is not a valid number`);
+        return;
+    }
+
+    // execute core logic
+    average(newNumber);
+
+    // communicate result to user
+    const message = `running average: ${data.average}`;
+    alert(message);
+
+    // log interaction
+    console.log(data);
+};
