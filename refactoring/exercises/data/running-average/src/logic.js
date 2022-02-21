9441aad86c41fe53b3f4205a3098fe097f6e6dde
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-21 19:28:56 +0100

    Update logic.js

--- refactoring/exercises/data/running-average/src/logic.js
+++ refactoring/exercises/data/running-average/src/logic.js
@@ -1,4 +1,10 @@
 /**
  *
  */
-export const average = () => {};
+
+import { data } from './data.js';
+
+export const average = (userInput) => {
+    data.numbers.push(userInput);
+    data.average = data.numbers.reduce((sum, next) => sum + next, 0) / data.numbers.length;
+};
