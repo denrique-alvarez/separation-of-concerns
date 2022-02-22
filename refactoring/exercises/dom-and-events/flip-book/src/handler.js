b2d0b3182fbcd2e2470641ee5bceb98dabf2298d
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-22 10:27:39 +0100

    Update handler.js

--- refactoring/exercises/dom-and-events/flip-book/src/handler.js
+++ refactoring/exercises/dom-and-events/flip-book/src/handler.js
@@ -1,3 +1,19 @@
-// import { reverseAndUpper } from './utils.js';
+import { reverseAndUpper } from './logic.js';
 
-export const reverseAndUpperHandler = _;
+export const reverseAndUpperHandler = (event) => {
+    // debugger;
+    // read and process user input
+    const input = event.target.value;
+
+    // execute core logic
+
+    const reversedUppercase = reverseAndUpper(input);
+
+    // render result for user
+    document.getElementById('output').innerHTML = reversedUppercase;
+
+    // log result for developers
+    console.log('\n --- user action ---');
+    console.log('input:', input);
+    console.log('reversedUppercase:', reversedUppercase);
+};
