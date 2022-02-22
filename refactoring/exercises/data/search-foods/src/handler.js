d61fe3f694c2ba683a14a305c742ce8a97727e7c
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-22 09:34:15 +0100

    Update handler.js

--- refactoring/exercises/data/search-foods/src/handler.js
+++ refactoring/exercises/data/search-foods/src/handler.js
@@ -1,4 +1,20 @@
 import { search } from './utils.js';
 import { data } from './data.js';
 
-export const seachFoodsHandler = () => {};
+export const searchFoodsHandler = () => {
+    // read & process user input
+    const query = prompt('enter a search query');
+    if (query === null || query === '') {
+        return;
+    }
+
+    // execute core logic
+    search(query);
+
+    // communicate result to user
+    const message = `foods matching "${query}":${results}`;
+    alert(message);
+
+    // log the interaction
+    console.log(query);
+};
