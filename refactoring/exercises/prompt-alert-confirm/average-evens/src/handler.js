d1733ded3696f91d8b0ada392b6c5c1b31c12876
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-22 17:30:41 +0100

    Update handler.js

--- refactoring/exercises/prompt-alert-confirm/average-evens/src/handler.js
+++ refactoring/exercises/prompt-alert-confirm/average-evens/src/handler.js
@@ -1,3 +1,29 @@
-import { averageEvens } from './utils.js';
+import { averageEvens } from './logic.js';
 
-export const averageEvensHandler = () => {};
+export const averageEvensHandler = () => {
+    // read & process user input
+    const allNumbers = [];
+    let acceptingInput = true;
+    while (acceptingInput) {
+        const nextInput = prompt('enter a number');
+        if (nextInput === null || nextInput === '') {
+            acceptingInput = false;
+            continue;
+        }
+
+        const nextNumber = Number(nextInput);
+        if (Number.isNaN(nextNumber)) {
+            alert(`"${nextInput}" is not a valid number`);
+        } else {
+            allNumbers.push(nextNumber);
+        }
+    }
+
+    // execute core logic
+    const average = averageEvens(allNumbers);
+
+    // communicate result to user
+    const message = `average of all evens: ${average}`;
+    alert(message);
+
+};
