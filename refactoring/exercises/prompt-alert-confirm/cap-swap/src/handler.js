49883d00f21620d76326ffbf168da1a5f7b33a17
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-22 19:03:40 +0100

    Update handler.js

--- refactoring/exercises/prompt-alert-confirm/cap-swap/src/handler.js
+++ refactoring/exercises/prompt-alert-confirm/cap-swap/src/handler.js
@@ -1,3 +1,15 @@
-import { capSwap } from './utils.js';
+import { capSwap } from './logic.js';
 
-export const capSwapHandler = () => {};
+export const capSwapHandler = () => {
+    // read & process user input
+    let userInput = null;
+    while (userInput === null) {
+        userInput = prompt('enter a string to cap-swap');
+    }
+
+    // execute core logic
+    let capSwapped = capSwap(userInput);
+
+    // communicate result to user
+    alert(capSwapped);
+};
