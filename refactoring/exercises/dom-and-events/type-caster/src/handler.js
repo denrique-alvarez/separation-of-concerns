2fe1a29c58cf4deb86e3c17ecf0294277f28c124
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-22 13:53:55 +0100

    Update handler.js

--- refactoring/exercises/dom-and-events/type-caster/src/handler.js
+++ refactoring/exercises/dom-and-events/type-caster/src/handler.js
@@ -1,3 +1,22 @@
-import { typeCaster } from './utils.js';
+import { typeCaster } from './logic.js';
 
-export const castTheValue = () => {};
+export const castTheValue = (event) => {
+    // debugger;
+    // read & process user input
+    const form = event.target.form;
+    const intendedType = form.type.value;
+    const stringToCast = form.value.value;
+
+    // execute core logic
+    const newValue = typeCaster(stringToCast, intendedType);
+
+    // communicate result to user
+    document.getElementById('casted-value').innerHTML =
+        typeof newValue + ': ' + newValue;
+
+    // log action for developers
+    console.log('\n-- user action --');
+    console.log('stringToCast:', stringToCast);
+    console.log('intendedType:', intendedType);
+    console.log('newValue:', newValue);
+};
