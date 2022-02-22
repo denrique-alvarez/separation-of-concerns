d086fe3c70331d7ea0d4958ad9c38e9b7e822b98
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-22 09:48:09 +0100

    Update logic.js

--- refactoring/exercises/data/search-foods/src/logic.js
+++ refactoring/exercises/data/search-foods/src/logic.js
@@ -4,11 +4,8 @@
 
 import { data } from './data.js';
 
-export const search = (query) => {
-    const results = data.foods
-    .filter((food) => food.includes(query))
-    .map((food) => `\n- ${food}`)
-    .join('');
+export const search = (data, query) => {
+    const results = data.foods.filter((food) => food.includes(query)).map((food) => `\n- ${food}`).join('');
 
     return results;
 };
