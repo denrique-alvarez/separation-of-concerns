4f0fc4681ac1601f16afcb758d0cbe2bd87dec23
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-24 10:14:34 +0100

    Create logic.js

---
+++ reverse-engineering/level-1/color-palate/src/logic/logic.js
@@ -0,0 +1,11 @@
+/**
+ *
+ *
+ *
+ *
+ */
+
+export const applyColor = (newColor) => {
+    const newBgColor = document.getElementById('user-interface').style.backgroundColor = newColor;
+    return newBgColor;
+}
